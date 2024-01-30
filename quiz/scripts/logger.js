04787e533fb5eb4ce4d41cbbb24666ecfd75d273
Rachel Dao <rachelqdao@gmail.com>
2024-01-30 13:20:01 -0500

    fixed vars3, logger, arrow, clossure, addevent

--- quiz/scripts/logger.js
+++ quiz/scripts/logger.js
@@ -1,1 +1,16 @@
 // Define a JavaScript function called logMsg() that can be used to log an error message for any object that contains the property errMsg. 
+
+function logMsg(){
+    console.log(`Error Message: ${this.errMsg}`)
+}
+
+const o1 = {
+    errMsg: '01'
+}
+
+const o2 = {
+    errMsg: '02'
+}
+
+logMsg.call(o1);
+logMsg.call(o2);
